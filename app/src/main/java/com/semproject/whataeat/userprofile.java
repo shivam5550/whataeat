ac96b96701f65c7e5ee0dc81280d528878e3e389
shivam5550 <sharma.shivamhr@gmail.com>
2020-04-20 14:34:42 +0530

    WhataEat!

---
+++ app/src/main/java/com/semproject/whataeat/userprofile.java
@@ -0,0 +1,14 @@
+package com.semproject.whataeat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class userprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+    }
+}
